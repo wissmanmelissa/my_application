79300045de9b0b97ba55d02789dbdf56a079155a
mwissman <mwissman@bnl.gov>
2023-06-05 15:43:18 -0700

    Start screen added to allow application to create all necessary local data storage BEFORE user/application tries to access data

---
+++ app/src/main/java/com/example/myapplication/StartScreen.java
@@ -0,0 +1,77 @@
+package com.example.myapplication;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.TypedValue;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.example.myapplication.DataObjects.DataOrganizer;
+import com.example.myapplication.SearchScreens.MainScreen;
+
+public class StartScreen extends AppCompatActivity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        /*Activity that generates an Splash
+        page while the application initializes
+        the local storage and captures the screen
+        size*/
+
+        //Activity's Context
+        Context mContext = this;
+
+        //generate screen display using "start screen" XML file
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.startscreen);
+
+        //set toolbar height to 1/10th size of screen and set as Activity's ActionBar
+        int actionBarSize = new WindowSizing().returnActionBarSize();
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.getLayoutParams().height = actionBarSize;
+        setSupportActionBar(toolbar);
+        //set default actionbar title to invisible
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        //set title height to 1/16th size of screen
+        int widgetSize = new WindowSizing().returnWidgetSize();
+        TextView title = (TextView)findViewById(R.id.title);
+        title.setTextSize(TypedValue.COMPLEX_UNIT_PX, widgetSize);
+
+
+        //thread used to create all local storage of data
+        Thread welcomeThread = new Thread()
+        {
+            @Override
+            public void run()
+            {
+                try
+                {
+                    super.run();
+                    DataOrganizer data = new DataOrganizer(mContext);
+                    data.getAuthorMap();
+                    data.getBooks();
+                    data.getSubjectMap();
+                }
+                catch (Exception e)
+                {
+                    Log.e("fail", e.toString());
+                }
+                finally
+                {
+                    /*once local storage of data created, user moved to main screen*/
+                    Intent i = new Intent(StartScreen.this, MainScreen.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+
+        //start thread
+        welcomeThread.start();
+    }
+}
