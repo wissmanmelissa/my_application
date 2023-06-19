1a49842719d60a59eccd890c8986956964f9ffb3
mwissman <mwissman@bnl.gov>
2023-06-19 09:25:25 -0700

    Unit tests developed to ensure Book Objects are developed properly

--- app/src/test/java/com/example/myapplication/ExampleUnitTest.java
+++ app/src/test/java/com/example/myapplication/ExampleUnitTest.java
@@ -4,14 +4,45 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import com.example.myapplication.DataObjects.Book;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest {
+public class ExampleUnitTest
+{
+    Book exampleBook1 = Book.createBook("Gone With the Wind", "1936-06-30", "PAPERBACK", "FICTION", new String[]{"Margret Mitchell"});
+    Book exampleBook2 = Book.createBook("The Phantom Tollbooth", "1961-09-30", "VIDEO", "SCIENCE FICTION", new String[]{"Norton Juster"});
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void checkBookTitle()
+    {
+        assertEquals("Gone With the Wind", exampleBook1.returnTitle());
+    }
+
+    @Test
+    public void checkBookDate()
+    {
+        assertEquals("1936-06-30", exampleBook1.returnDate());
+    }
+
+    @Test
+    public void checkBookFormat()
+    {
+        assertEquals("PAPERBACK", exampleBook1.returnFormat());
+    }
+
+    @Test
+    public void checkBookSubject()
+    {
+        assertEquals("FICTION", exampleBook1.returnSubject());
+    }
+
+    @Test
+    public void invalidBook()
+    {
+        assertNull(exampleBook2);
     }
 }
