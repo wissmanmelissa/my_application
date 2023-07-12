66692ce29f3ca4af016a3aa9b25afbededca5aaa
mwissman <mwissman@bnl.gov>
2023-07-11 18:19:17 -0700

    Unit tests modified to account for changing of Book class' date variable from String to LocalDate variable

--- app/src/test/java/com/example/myapplication/ExampleUnitTest.java
+++ app/src/test/java/com/example/myapplication/ExampleUnitTest.java
@@ -6,6 +6,8 @@ import static org.junit.Assert.*;
 
 import com.example.myapplication.DataObjects.Book;
 
+import java.time.LocalDate;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -13,18 +15,13 @@ import com.example.myapplication.DataObjects.Book;
  */
 public class ExampleUnitTest
 {
-    Book exampleBook1 = Book.createBook("Gone With the Wind", "1936-06-30", "PAPERBACK", "FICTION", new String[]{"Margret Mitchell"});
-
-    @Test
-    public void checkBookTitle()
-    {
-        assertEquals("Gone With the Wind", exampleBook1.returnTitle());
-    }
+    Book exampleBook1 = Book.createBook("Gone With the Wind", "1936" , "06", "30", "PAPERBACK", "FICTION", new String[]{"Margret Mitchell"});
 
     @Test
     public void checkBookDate()
     {
-        assertEquals("1936-06-30", exampleBook1.returnDate());
+
+        assertEquals(LocalDate.of(1936, 06, 30), exampleBook1.returnDate());
     }
 
     @Test
@@ -42,21 +39,21 @@ public class ExampleUnitTest
     @Test
     public void invalidFormat()
     {
-        Book exampleBook2 = Book.createBook("The Phantom Tollbooth", "1961-09-30", "VIDEO", "FICTION", new String[]{"Norton Juster"});
+        Book exampleBook2 = Book.createBook("The Phantom Tollbooth", "1961", "09", "30", "VIDEO", "FICTION", new String[]{"Norton Juster"});
         assertNull(exampleBook2);
     }
 
     @Test
     public void invalidSubject()
     {
-        Book exampleBook2 = Book.createBook("The Phantom Tollbooth", "1961-09-30", "AUDIO", "SCIENCE FICTION", new String[]{"Norton Juster"});
+        Book exampleBook2 = Book.createBook("The Phantom Tollbooth", "1961", "09","30", "AUDIO", "SCIENCE FICTION", new String[]{"Norton Juster"});
         assertNull(exampleBook2);
     }
 
     @Test
     public void invalidAuthor()
     {
-        Book exampleBook2 = Book.createBook("The Phantom Tollbooth", "1961-09-30", "AUDIO", "FICTION", new String[]{"Norton Juster", "Frank"});
+        Book exampleBook2 = Book.createBook("The Phantom Tollbooth", "1961" , "09", "30", "AUDIO", "FICTION", new String[]{"Norton Juster", "Frank"});
         assertNull(exampleBook2);
     }
 }
